6cc0b58fb0f96c4ef58424be64dec8da93938bb2
chathura Dissanayake <cdchathura@gmail.com>
2020-06-28 15:12:36 +0800

    init commit

---
+++ src/main/java/com/cha/edu/rest/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.cha.edu.rest.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
